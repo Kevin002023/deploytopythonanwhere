78cc3fd1721292491579a8af13ec866e6b4a4dd1
Kevin O'Leary <rpskoleary@gmail.com>
2024-05-09 20:09:13 +0100

    fixed server

--- flask_server.py
+++ flask_server.py
@@ -7,9 +7,9 @@ from brokerDAO import brokerDAO
 #create flask application 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
-@app.route('/')
-def index():
-    return "Hello, Kevin!"
+#@app.route('/')
+#def index():
+#    return "Hello, Kevin!"
 
 #return all brokers
 
